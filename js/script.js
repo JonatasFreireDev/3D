4d984de056590db7ec82727410fd3ad6782d5fff
Jonatas Freire <jonatasf1997@hotmail.com>
2019-11-13 00:35:16 -0300

    add features, add new cubs, change they colors

--- js/script.js
+++ js/script.js
@@ -12,17 +12,6 @@ var DistanciaEXM = document.getElementById("DistanciaEXM");
 var DistanciaEYM = document.getElementById("DistanciaEYM");
 var DistanciaEZM = document.getElementById("DistanciaEZM");
 
-var translatX = 0.1; 
-var translatY = 0.1; 
-var translatZ = 0.1; 
-
-DistanciaEXS.onclick = () => {translatX = 0; translatX += 0.1; geometry.translate(translatX,0,0);}
-DistanciaEYS.onclick = () => {translatY = 0; translatY += 0.1; geometry.translate(0,translatY,0);}
-DistanciaEZS.onclick = () => {translatZ = 0; translatZ += 0.1; geometry.translate(0,0,translatZ);}
-DistanciaEXM.onclick = () => {translatX = 0; translatX -= 0.1; geometry.translate(translatX,0,0);}
-DistanciaEYM.onclick = () => {translatY = 0; translatY -= 0.1; geometry.translate(0,translatY,0);}
-DistanciaEZM.onclick = () => {translatZ = 0; translatZ -= 0.1; geometry.translate(0,0,translatZ);}
-
 //Altera a Rotação do Quadrado ao clicar em botoes
 var RotationEXS = document.getElementById("RotationEXS");
 var RotationEYS = document.getElementById("RotationEYS");
@@ -35,16 +24,30 @@ var RotationFullX = document.getElementById("RotationFullX");
 var RotationFullY = document.getElementById("RotationFullY");
 var RotationFullZ = document.getElementById("RotationFullZ");
 
-//Altera cor do Quadrado
+var RotationFullMX = document.getElementById("RotationFullMX");
+var RotationFullMY = document.getElementById("RotationFullMY");
+var RotationFullMZ = document.getElementById("RotationFullMZ");
+
+var AddCuboX = document.getElementById("AddCuboX");
+var AddCuboZ = document.getElementById("AddCuboZ");
+var AddCuboXZ = document.getElementById("AddCuboXZ");
+
 var inputColor = document.getElementById("inputColor");
 
-inputColor.onchange = event => {
-  event.preventDefault();
-  scene.remove( cube );
-  material = new THREE.MeshBasicMaterial( {color: `${inputColor.value}`, wireframe: true} );
-  cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
-}
+
+//Cria os cubos Reflexos
+var cubeX, cubeXZ, cubeZ;
+
+var translatX = 0.1; 
+var translatY = 0.1; 
+var translatZ = 0.1; 
+
+DistanciaEXS.onclick = () => {translatX = 0;translatX += 0.1;cube.geometry.translate(translatX,0,0);}
+DistanciaEYS.onclick = () => {translatY = 0;translatY += 0.1;cube.geometry.translate(0,translatY,0);}
+DistanciaEZS.onclick = () => {translatZ = 0;translatZ += 0.1;cube.geometry.translate(0,0,translatZ);}
+DistanciaEXM.onclick = () => {translatX = 0;translatX -= 0.1;cube.geometry.translate(translatX,0,0);}
+DistanciaEYM.onclick = () => {translatY = 0;translatY -= 0.1;cube.geometry.translate(0,translatY,0);}
+DistanciaEZM.onclick = () => {translatZ = 0;translatZ -= 0.1;cube.geometry.translate(0,0,translatZ);}
 
 // Cria scene
 var scene = new THREE.Scene();
@@ -72,20 +75,68 @@ var controls = new THREE.OrbitControls( camera, renderer.domElement );
 camera.position.set( 8, 15, 8 );
 controls.update();
 
-//Cria um cubo
-var geometry = new THREE.BoxGeometry( 1, 1, 1, 4, 4, 4);
-geometry.translate(0.5,0.5,0.5);
-var material = new THREE.MeshBasicMaterial( {color: 'black', wireframe: true} );
-var cube = new THREE.Mesh( geometry, material );
+//Função para criar Cubo
+function createCube(x, y, z, color="black"){
+  var geometry = new THREE.BoxGeometry( 1, 1, 1, 4, 4, 4);
+  geometry.translate(x,y,z);
+  var material = new THREE.MeshBasicMaterial( {color: color, wireframe: true} );
+  var cube = new THREE.Mesh( geometry, material );
+  return cube;
+}
+
+//Cria e remove o cubo da cena
+function addCube(cube, x, y, z, color){
+  if(cube){
+    scene.remove(cube);
+    cube = undefined;
+    return false;
+  }else{
+    var cube = createCube(x, y, z, color);
+    scene.add(cube);
+    return cube;
+  }
+}
+
+//Cria o cubo Principal
+var cube = createCube(0.5,0.5,0.5);
+
+AddCuboX.onclick = () => {cubeX = addCube(cubeX,-0.5,0.5,0.5);}
+AddCuboXZ.onclick = () => {cubeXZ = addCube(cubeXZ,-0.5,0.5,-0.5);}
+AddCuboZ.onclick = () => {cubeZ = addCube(cubeZ, 0.5,0.5,-0.5);}
+
+//Altera cor do Quadrado
+inputColor.onchange = event => {
+  event.preventDefault();
+
+  if(cubeX){
+    scene.remove( cubeX );
+    cubeX = createCube(-0.5,0.5,0.5, inputColor.value);
+    scene.add(cubeX);
+  }
+  if(cubeXZ){
+    scene.remove( cubeXZ );
+    cubeXZ = createCube(-0.5,0.5,-0.5, inputColor.value);
+    scene.add(cubeXZ);
+  }
+  if(cubeZ){
+    scene.remove( cubeZ );
+    cubeZ = createCube(0.5,0.5,-0.5, inputColor.value);
+    scene.add(cubeZ);
+  }
+
+  scene.remove( cube );
+  cube = createCube(0.5,0.5,0.5, inputColor.value);
+  scene.add(cube);
+}
 
 //Adiciona a Scene
-scene.add( cube );
 scene.add( new THREE.AxesHelper( 6 ) );
+scene.add(cube);
 
 var animate = function () {
     requestAnimationFrame( animate );
 
-    //Altera a Rotação do Quadrado ao clicar em botoes
+    //Altera a Rotação do Quadrado ao clicar em botoe
     RotationEXS.onclick = () => {cube.rotation.x += 0.03;}
     RotationEYS.onclick = () => {cube.rotation.y += 0.03;}
     RotationEZS.onclick = () => {cube.rotation.z += 0.03;}
@@ -93,22 +144,17 @@ var animate = function () {
     RotationEYM.onclick = () => {cube.rotation.y -= 0.03;}
     RotationEZM.onclick = () => {cube.rotation.z -= 0.03;}
 
-    if(RotationFullX.checked){
-      cube.rotation.x += 0.01;
-    }
 
-    if(RotationFullY.checked){
-      cube.rotation.y += 0.01;
-    }
-
-    if(RotationFullZ.checked){
-      cube.rotation.z += 0.01;
-    }
-
+    if(RotationFullX.checked){cube.rotation.x += 0.01;}
+    if(RotationFullY.checked){cube.rotation.y += 0.01;}
+    if(RotationFullZ.checked){cube.rotation.z += 0.01;}
+    if(RotationFullMX.checked){cube.rotation.x -= 0.01;}
+    if(RotationFullMY.checked){cube.rotation.y -= 0.01;}
+    if(RotationFullMZ.checked){cube.rotation.z -= 0.01;}
     
     angle.innerHTML = "X:" + Math.floor(THREE.Math.radToDeg(cube.rotation.x) % 360) + "º";
-    angle.innerHTML +="<br> Y:"+ Math.floor(THREE.Math.radToDeg(cube.rotation.y) % 360) + "º";
-    angle.innerHTML +="<br> Z:"+ Math.floor(THREE.Math.radToDeg(cube.rotation.z) % 360) + "º";
+    angle.innerHTML +=" Y:"+ Math.floor(THREE.Math.radToDeg(cube.rotation.y) % 360) + "º";
+    angle.innerHTML +=" Z:"+ Math.floor(THREE.Math.radToDeg(cube.rotation.z) % 360) + "º";
 
     renderer.render( scene, camera );
 };
